bfe1d32127b8dac8d332d94e0016fdb60ed06592
Rob Clark <rob@robclark.io>
2023-08-08 13:36:18 -0500

    Initial release

---
+++ feeds/podcast-feed-template.php
@@ -0,0 +1,72 @@
+<?php
+header('Content-Type: text/xml');
+
+$options = get_option( 'method_podcast' );
+$util = new Method_Podcast_Utilities();
+
+echo '<?xml version="1.0" encoding="UTF-8"?><rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0"><channel>';
+echo ( $util->get_option( 'channel_title' ) ? '<title>' . $util->get_option( 'channel_title' ) . '</title>' : '' );
+echo ( $util->get_option( 'channel_desc' ) ? '<description><![CDATA[' . apply_filters( 'the_content', $util->get_option( 'channel_desc' ) ) . ']]></description>' : '' );
+if ( $util->get_option( 'channel_artwork' ) ) {
+	echo '<itunes:image href="' . $util->get_option( 'channel_artwork' ) . '"/>';
+	if ( ( $util->get_option( 'channel_title' ) ) && ( $util->get_option( 'channel_link' ) ) ) {
+		echo '<image><url>' . $util->get_option( 'channel_artwork' ) . '</url><title>' . $util->get_option( 'channel_title' ) . '</title><link>' . $util->get_option( 'channel_link' ) . '</link></image>';
+	}
+}
+echo ( $util->get_option( 'channel_language' ) ? '<language>' . $util->get_option( 'channel_language' ) . '</language>' : '' );
+if ( $util->get_option( 'channel_itunes_categories' ) ) {
+	if ( is_array( $util->get_option( 'channel_itunes_categories' ) ) ) {
+		if ( 0 < count( $util->get_option( 'channel_itunes_categories' ) ) ) {
+			foreach ( $util->get_option( 'channel_itunes_categories' ) as $cat ) {
+				if ( ! empty( $cat ) ) {
+					$cata = explode( ", ", $cat );
+					if ( 1 < count( $cata ) ) {
+						echo '<itunes:category text="' . $cata[0] . '"><itunes:category text="' . $cata[1] . '"/></itunes:category>';
+					} elseif ( 1 == count( $cata ) ) {
+						echo '<itunes:category text="' . $cata[0] . '"/>';
+					} else {
+						// Welp, no categories
+					}
+				}
+			}
+		}
+	}
+}
+echo ( $util->get_option( 'channel_explicit' ) ? '<itunes:explicit>' . ( 'on' == $util->get_option( 'channel_explicit' ) ? 'true' : 'false' ) . '</itunes:explicit>' : '' );
+echo ( $util->get_option( 'channel_author' ) ? '<itunes:author>' . $util->get_option( 'channel_author' ) . '</itunes:author>' : '' );
+if ( ( $util->get_option( 'channel_owner_email' ) ) && ( $util->get_option( 'channel_owner_name' ) ) ) {
+	echo '<itunes:owner><itunes:name>' . $util->get_option( 'channel_owner_name' ) . '</itunes:name><itunes:email>' . $util->get_option( 'channel_owner_email' ) . '</itunes:email></itunes:owner>';
+}
+echo ( $util->get_option( 'channel_copyright' ) ? '<copyright>' . $util->get_option( 'channel_copyright' ) . '</copyright>' : '' );
+echo ( $util->get_option( 'channel_link' ) ? '<link>' . $util->get_option( 'channel_link' ) . '</link>' : '' );
+$args = array(
+	'post_type' => 'method_podcast',
+	'posts_per_page' => -1,
+	'post_status' => 'publish',
+	'order' => 'DESC',
+	'fields' => 'ids',
+);
+$items = get_posts( $args );
+if ( $items ) {
+	if ( is_array( $items ) ) {
+		if ( 0 < count( $items ) ) {
+			foreach ( $items as $item ) {
+				$util->load_meta( $item );
+				$desc = $util->get_loaded_meta( '_method_podcast_desc' );
+				echo '<item>';
+				echo '<title>' . get_the_title( $item ) . '</title>';
+				echo '<guid>' . $item . '</guid>';
+				echo ( $util->get_loaded_meta( '_method_podcast_artwork' ) ? '<itunes:image href="' . $util->get_loaded_meta( '_method_podcast_artwork' ) . '"/>' : '' );
+				echo '<itunes:explicit>' . ( 'on' == $util->get_loaded_meta( '_method_podcast_explicit' ) ? 'true' : 'false' ) . '</itunes:explicit>';
+				echo '<description><![CDATA[' . apply_filters( 'the_content', ( ! empty( $desc ) ? $desc : get_the_content( null, false, $item ) ) ) . ']]></description>';
+				if ( ( $util->get_loaded_meta( '_method_podcast_file' ) ) && ( $util->get_loaded_meta( '_method_podcast_length' ) ) ) {
+					echo '<enclosure url="' . $util->get_loaded_meta( '_method_podcast_file' ) . '" length="' . $util->get_loaded_meta( '_method_podcast_length' ) . '" type="' . $util->get_loaded_meta( '_method_podcast_type', 'audio/mpeg' ) . '" />';
+				}
+				echo ( $util->get_loaded_meta( '_method_podcast_duration' ) ? '<itunes:duration>' . $util->get_loaded_meta( '_method_podcast_duration' ) . '</itunes:duration>' : '' );
+				echo '</item>';
+				$util->unload_meta();
+			}
+		}
+	}
+}
+echo '</channel></rss>';
--- method-podcast.php
+++ method-podcast.php
@@ -10,6 +10,16 @@
  * Text Domain: method-podcast
  */
 
+function method_podcast_add_feed() {
+  	add_feed('podcast-feed', 'method_podcast_load_feed');
+}
+add_action('init', 'method_podcast_add_feed');
+
+function method_podcast_load_feed() {
+  	add_filter('pre_option_rss_use_excerpt', '__return_zero');
+  	load_template( plugin_dir_path( __FILE__ ) . 'feeds/podcast-feed-template.php' );
+}
+
 add_action( 'cmb2_admin_init', 'method_podcast_options_metabox' );
 
 function method_podcast_options_metabox() {
@@ -269,4 +279,290 @@ function method_podcast_options_metabox() {
 		)
 	);
 
+}
+
+add_action( 'init', 'method_podcast_init' );
+
+function method_podcast_init() {
+	$labels = array(
+		'name'               => _x( 'Podcasts', 'post type general name', 'hfh' ),
+		'singular_name'      => _x( 'Podcast', 'post type singular name', 'hfh' ),
+		'menu_name'          => _x( 'Podcasts', 'admin menu', 'hfh' ),
+		'name_admin_bar'     => _x( 'Podcast', 'add new on admin bar', 'hfh' ),
+		'add_new'            => _x( 'Add News Podcast', 'job', 'hfh' ),
+		'add_new_item'       => __( 'Add New Podcast', 'hfh' ),
+		'new_item'           => __( 'New Podcast', 'hfh' ),
+		'edit_item'          => __( 'Edit Podcast', 'hfh' ),
+		'view_item'          => __( 'View Podcast', 'hfh' ),
+		'all_items'          => __( 'Podcasts', 'hfh' ),
+		'search_items'       => __( 'Search Podcasts', 'hfh' ),
+		'parent_item_colon'  => __( 'Parent Podcast:', 'hfh' ),
+		'not_found'          => __( 'No podcasts found.', 'hfh' ),
+		'not_found_in_trash' => __( 'No podcasts found in Trash.', 'hfh' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'description'        => __( 'A description for the post type.', 'hfh' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'query_var'          => true,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position' 	 => 5,
+		'menu_icon'			 => 'dashicons-megaphone',
+		'supports'           => array( 'title' , 'editor' )
+	);
+
+	register_post_type( 'method_podcast', $args );
+}
+
+add_action( 'cmb2_admin_init', 'method_podcast_register_podcast_metabox' );
+
+function method_podcast_register_podcast_metabox() {
+	$cmb_options = new_cmb2_box(
+		array(
+			'id'            => '_method_postcast_metabox_podcast',
+			'title'         => esc_html__( 'Podcast Options', 'cmb2' ),
+			'object_types'  => array( 'method_podcast' ),
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Description', 'method-podcast' ),
+			'desc'     => __( 'Provide an episode description.', 'hfh' ),
+			'id'       => '_method_podcast_desc',
+			'type'     => 'wysiwyg',
+			'options'  => array(
+				'media_buttons' => false,
+				'textarea_rows' => 8,
+			)
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Podcast Artwork', 'method-podcast' ),
+			'desc'     => __( 'Provide artwork for this episode if desired. Otherwise, the channel artwork will be used. Artwork must be a minimum size of 1400 x 1400 pixels and a maximum size of 3000 x 3000 pixels, in JPEG or PNG format, 72 dpi, with appropriate file extensions (.jpg, .png), and in the RGB colorspace. These requirements are different from the standard RSS image tag specifications.', 'hfh' ),
+			'id'       => '_method_podcast_artwork',
+			'type'     => 'file',
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Explicit?', 'method-podcast' ),
+			'desc'     => __( 'Check this box if this episode contains explicit content.', 'hfh' ),
+			'id'       => '_method_podcast_explicit',
+			'type'     => 'checkbox',
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Audio File', 'method-podcast' ),
+			'desc'     => __( 'Provide the audio file for this episode.', 'hfh' ),
+			'id'       => '_method_podcast_file',
+			'type'     => 'file',
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Audio File', 'method-podcast' ),
+			'desc'     => __( 'Provide the audio file for this episode.', 'hfh' ),
+			'id'       => '_method_podcast_file',
+			'type'     => 'file',
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Audio File Length', 'method-podcast' ),
+			'desc'     => __( 'This value will automatically be calculated and most likely will never need to be manually entered.', 'hfh' ),
+			'id'       => '_method_podcast_length',
+			'type'     => 'text',
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Audio File Type', 'method-podcast' ),
+			'desc'     => __( 'This will default to the filetype for mp3, but can be manually changed if needed.', 'hfh' ),
+			'id'       => '_method_podcast_filetype',
+			'type'     => 'text',
+			'attributes' => array(
+				'placeholder' => 'audio/mpeg',
+			),
+		)
+	);
+
+	$cmb_options->add_field(
+		array(
+			'name'     => __( 'Audio File Duration', 'method-podcast' ),
+			'desc'     => __( 'Provide the duration of this episode, in seconds.', 'hfh' ),
+			'id'       => '_method_podcast_duration',
+			'type'     => 'text',
+		)
+	);
+
+}
+
+function method_podcast_update_filelength( $id, $post ) {
+	$file = get_post_meta( $post->ID, '_method_podcast_file', true );
+
+	if ( !empty($file) ) {
+		$filesize = method_podcast_getRemoteFilesize( $file, false );
+		update_post_meta( $post->ID, '_method_podcast_length', $filesize ); 
+	}
+
+	return;
+}
+
+add_action( 'save_post', 'method_podcast_update_filelength', 10000, 2 );
+
+function method_podcast_getRemoteFilesize($file_url, $formatSize = true)
+{
+    $head = array_change_key_case(get_headers($file_url, 1));
+    // content-length of download (in bytes), read from Content-Length: field
+	
+    $clen = isset($head['content-length']) ? $head['content-length'] : 0;
+ 
+    // cannot retrieve file size, return "-1"
+    if (!$clen) {
+        return -1;
+    }
+ 
+    if (!$formatSize) {
+        return $clen; 
+		// return size in bytes
+    }
+ 
+    $size = $clen;
+    switch ($clen) {
+        case $clen < 1024:
+            $size = $clen .' B'; break;
+        case $clen < 1048576:
+            $size = round($clen / 1024, 2) .' KB'; break;
+        case $clen < 1073741824:
+            $size = round($clen / 1048576, 2) . ' MB'; break;
+        case $clen < 1099511627776:
+            $size = round($clen / 1073741824, 2) . ' GB'; break;
+    }
+ 
+    return $size; 
+	// return formatted size
+}
+
+function method_podcast_check_array_key( $item, $key ) {
+	$output = false;
+	if ( is_array( $item ) ) {
+		if ( array_key_exists( $key, $item ) ) {
+			if ( ! empty( $item["{$key}"] ) ) {
+				$output = true;
+			}
+		}
+	}
+	return $output;
+}
+
+class Method_Podcast_Utilities {
+	protected $loaded_meta = array();
+	protected $opts        = array();
+
+	function __construct() {
+		$this->opts = get_option( 'method_podcast' );
+	}
+
+	public function load_meta( $id ) {
+		$this->loaded_meta = get_post_meta( $id );
+		return;
+	}
+
+	public function unload_meta() {
+		$this->loaded_meta = array();
+		return;
+	}
+
+	public function get_loaded_meta( $key, $fallback = '' ) {
+		$output = false;
+		if ( $this->check_array_key( $this->loaded_meta, $key ) ) {
+			if ( $this->check_array_key( $this->loaded_meta[ "{$key}" ], 0 ) ) {
+				$output = $this->loaded_meta[ "{$key}" ][0];
+			}
+		}
+		return ( false === $output ? ( ! empty( $fallback ) ? $fallback : false ) : $output );
+	}
+
+	public function get_serialized_loaded_meta( $key ) {
+		$output = false;
+		if ( $this->check_array_key( $this->loaded_meta, $key ) ) {
+			if ( $this->check_array_key( $this->loaded_meta[ "{$key}" ], 0 ) ) {
+				$output = maybe_unserialize( $this->loaded_meta[ "{$key}" ][0] );
+			}
+		}
+		return $output;
+	}
+
+	public function get_loaded_headline( $key, $before, $after, $fallback = '' ) {
+		$output = '';
+		if ( ( $this->get_loaded_meta( $key ) ) || ( ! empty( $fallback ) ) ) {
+			$output = $before . ( $this->get_loaded_meta( $key ) ? $this->format_tags( esc_html( $this->get_loaded_meta( $key ) ) ) : $fallback ) . $after;
+		}
+		return $output;
+	}
+
+	public function get_loaded_content( $key, $before, $after, $fallback = '' ) {
+		$output = '';
+		if ( ( $this->get_loaded_meta( $key ) ) || ( ! empty( $fallback ) ) ) {
+			$output = $before . ( $this->get_loaded_meta( $key ) ? $this->filter_content( $this->get_loaded_meta( $key ) ) : $fallback ) . $after;
+		}
+		return $output;
+	}
+
+	public function get_option( $key, $fallback = '' ) {
+		$output = false;
+		if ( $this->check_array_key( $this->opts, $key ) ) {
+			$output = $this->opts[ "{$key}" ];
+		}
+		return ( false === $output ? ( ! empty( $fallback ) ? $fallback : false ) : $output );
+	}
+
+	public function get_headline_from_option( $key, $before, $after, $fallback = '' ) {
+		$output = '';
+		if ( ( $this->get_option( $key ) ) || ( ! empty( $fallback ) ) ) {
+			$output = $before . ( $this->get_option( $key ) ? $this->format_tags( esc_html( $this->get_option( $key ) ) ) : $fallback ) . $after;
+		}
+		return $output;
+	}
+
+	public function get_content_from_option( $key, $before, $after, $fallback = '' ) {
+		$output = '';
+		if ( ( $this->get_option( $key ) ) || ( ! empty( $fallback ) ) ) {
+			$output = $before . ( $this->get_option( $key ) ? $this->filter_content( $this->get_option( $key ) ) : $fallback ) . $after;
+		}
+		return $output;
+	}
+
+	public function check_array_key( $item, $key ) {
+		$output = false;
+		if ( is_array( $item ) ) {
+			if ( array_key_exists( $key, $item ) ) {
+				if ( ! empty( $item["{$key}"] ) ) {
+					$output = true;
+				}
+			}
+		}
+		return $output;
+	}
+
+	public function filter_content( $content ) {
+		if ( ! empty( $content ) ) {
+			$content = apply_filters( 'the_content', $content );
+		}
+		return $content;
+	}
 }
